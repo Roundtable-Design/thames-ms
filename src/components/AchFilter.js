4b7247509f861385da45324c8665764bf9a94516
James <james.al.da.brooks@gmail.com>
2021-07-22 13:24:11 +0100

    Added two filters for type and subject

--- src/components/AchFilter.js
+++ src/components/AchFilter.js
@@ -1,19 +1,7 @@
 import React from 'react'
 import Select from 'react-select'
 
-const options = [
-  { value: null, label: 'None' },
-  { value: 'Competition', label: 'Competition' },
-  { value: 'Masterclass', label: 'Masterclass' },
-  { value: 'Online course', label: 'Online course' },
-  { value: 'Personal project', label: 'Personal project' },
-  { value: 'Reading', label: 'Reading' },
-  { value: 'Work experience', label: 'Work experience' },
-  { value: 'Other', label: 'Other' },
-]
-
-
-export default ({ setFilter }) => {
+export default ({ setFilter, options }) => {
 
   return (
     <Select options={options} onChange={({ value }) => setFilter(value)} />
--- src/components/AchStudentGrid.js
+++ src/components/AchStudentGrid.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
+import AchFilter from './AchFilter'
 
 
 const StudentWrapper = styled.div`
@@ -19,18 +20,54 @@ const AchWrapper = styled.div`
   border-radius: 3px;
 `
 
+const typeOptions = [
+  { value: null, label: 'None' },
+  { value: 'Competition', label: 'Competition' },
+  { value: 'Masterclass', label: 'Masterclass' },
+  { value: 'Online course', label: 'Online course' },
+  { value: 'Personal project', label: 'Personal project' },
+  { value: 'Reading', label: 'Reading' },
+  { value: 'Work experience', label: 'Work experience' },
+  { value: 'Other', label: 'Other' },
+]
+
+const subjectOptions = [
+  { value: null, label: 'None' },
+  { value: 'Art', label: 'Art' },
+  { value: 'Maths', label: 'Maths' },
+  { value: 'Biology', label: 'Biology' },
+  { value: 'Chemistry', label: 'Chemistry' },
+  { value: 'English', label: 'English' },
+  { value: 'Physics', label: 'Physics' },
+  { value: 'Computer Science', label: 'Computer Science'},
+  { value: 'History', label: 'History'},
+  { value: 'Geography', label: "Geography"},
+  { value: 'French', label: 'French'},
+  { value: 'Spanish', label: 'Spanish'},
+  { value: 'Buisness Studies', label: 'Buisness Studies'},
+  { value: 'Sport', label: 'Sport'},
+  { value: 'Other', label: 'Other' },
+]
+
+
 export default ({ students }) => {
+  const [ typeFilter, setTypeFilter ] = useState(null)
+  const [subjectFilter, setSubjectFilter ] = useState(null)
   return (
-    <StudentsWrapper>
-      {Students.map((student, i) =>
-        <StudentsColumn href = './achievements/${student.id}'>
-         <StudentName>{student.Name}</StudentName>
-         {student.Achievements.map((achievement, j) =>
-          <AchWrapper>
-            <h2 href = >{achievement.Name}</h2>
-            <p>{achievement.Date}</p>
-          </AchWrapper>
-         )}
+    <StudentsWrapper
+      <AchFilter options={typeOptions} setFilter={setTypeFilter}/>
+      <AchFilter options={subjectOptions} setFilter={setSubjectFilter}/>
+      {students.map((student, i) =>
+        <StudentsColumn href = `./achievements/${student.id}``>
+         <StudentName>{student.Forname} {student.Surname}</StudentName>
+          {student.Achievements.map((achievement, j) =>
+            (!typeFilter || achievement.Type == typeFilter)  &&
+            (!subjectFilter || achievement.Associations == subjectFilter) &&
+            <AchWrapper>
+              <h2>{achievement.Name}</h2>
+              <p>{achievement.Date}</p>
+            </AchWrapper>
+        )}
         </StudentsColumn>
       )}
     <StudentsWrapper/>
