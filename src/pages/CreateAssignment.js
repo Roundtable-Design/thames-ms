eae89b3c162d03204a3f92fb02fd061056c922aa
TKreateDev <tanbir98@hotmail.it>
2021-08-26 08:21:38 +0100

    added radio for event type and api for students

--- src/pages/CreateAssignment.js
+++ src/pages/CreateAssignment.js
@@ -20,13 +20,18 @@ export default () => {
 	const [submitLoading, setSubmitLoading] = React.useState(false);
 	const [classesLoading, setClassesLoading] =
 		React.useState("Loading classes...");
+	const [studentsLoading, setStudentsLoading] =
+		React.useState("Loading students...");
 	const [error, setError] = React.useState();
 	const [table, setTable] = React.useState();
-	const [isAssignment, setIsAssignment] = React.useState();
+	const [studentsTable, setStudentsTable] = React.useState();
+	const [eventType, setEventType] = React.useState();
+	const [classId, setClassId] = React.useState();
 	const [record, setRecord] = React.useState({});
+	
 
-	console.log("Hello?");
-
+	// console.log("Hello?");
+	const RadioEventTypes = ["Assignment", "Reminder", "Short Report", "Long Report"]
 	const handleSubmit = async (event) => {
 		event.preventDefault();
 
@@ -64,8 +69,18 @@ export default () => {
 		(async function () {
 			try {
 				console.log("Starting...");
-
 				const response = await API.get("classes");
+				const students = await API.get("students");
+				
+				if (!students.hasOwnProperty("content"))
+					throw new Error("Empty student response");
+
+				setStudentsTable(students.content);
+				console.log("This is the response for students", students.content);
+				setStudentsLoading(false);
+
+				console.log("Student table is here", { table: students.content });
+
 
 				if (!response.hasOwnProperty("content"))
 					throw new Error("Empty response");
@@ -90,34 +105,32 @@ export default () => {
 
 			<TeacherNav />
 			<Container>
-				<Heading>Create Assignment</Heading>
+				<Heading>Create</Heading>
 				<p>NOTE: Fields marked as * are required</p>
 				<Form onSubmit={handleSubmit}>
 					<Section loading={classesLoading} error={error} title="Type of event *">
-						<Form.Control
-							required
-							as="select"
+						{RadioEventTypes.map((type, key) =>
+						<Form.Check
+							key={key}
+							type="radio"
+							name="TypeOfEvent"
+							label={type}
 							onChange={({ target }) => {
-								setIsAssignment((target.value === 'assignment') ? true : false);
-								editRecord({
-									is_Reminder: (target.value === 'assignment') ? false : true
-								})
+								setEventType(key)
+								console.log(key);
 							}}
-						>
-							<option value="">-- Assign event type --</option>
-							<option value="assignment">Assignment</option>
-							<option value="reminder">Reminder</option>
-						</Form.Control>
+						/>
+						)}
 					</Section>
 					<Section loading={classesLoading} error={error} title="Class *">
 						<Form.Control
 							required
 							as="select"
 							onChange={({ target }) => {
-								const class_id =
-									target.options[target.selectedIndex].value;
-
-								console.log({ table, class_id });
+								let class_id = target.options[target.selectedIndex].value;
+								setClassId(class_id)
+								console.log(classId);
+								console.log({ table, class_id});
 
 								editRecord({
 									class_id: [class_id],
@@ -136,6 +149,30 @@ export default () => {
 								))}
 						</Form.Control>
 					</Section>
+					{(eventType == 2 || eventType == 3) &&
+					<Section loading={studentsLoading} error={error} title="Student *">
+						<Form.Control
+							required
+							as="select"
+							onChange={({ target }) => {
+								//change here
+								editRecord({
+									//change here
+								})	
+							}}
+						>
+							<option value="">-- Select a student --</option>
+							{(studentsTable) && 
+								studentsTable.filter(({fields}) => fields.class_year_id.includes(classId))
+								.map((({fields}) => 
+								<option value={fields.student_id}>
+									{fields.Forename}, {fields.Surname}
+								</option>
+								))
+							}
+						</Form.Control>
+					</Section>
+					}
 					<Section title="Content">
 						<p>To attach a file to this assignment add a link to a file on the school Google Drive</p>
 						<Form.Group>
@@ -181,7 +218,7 @@ export default () => {
 							</Col>
 						</Form.Row>
 					</Section>
-					{isAssignment &&
+					{!(eventType == 1) &&
 						<Section title="Expected time to complete assignment">
 							<Form.Row>
 								<Col>
