7110133c8635ad59661aae7465430c1c45dfb8a9
TKreateDev <tanbir98@hotmail.it>
2021-11-12 10:41:22 +0000

    enhanced sorting algorithm

--- src/pages/sections/ListofStudents.js
+++ src/pages/sections/ListofStudents.js
@@ -212,7 +212,7 @@ export default ({ query = null , classId}) => {
 					</thead>
 					<tbody>
 						{records &&
-							records.sort((a, b) => a.fields.Surname.localeCompare(b.fields.Surname)).map(({ fields, id }, index) => (
+							records.sort((a, b) => a.fields.Surname.toString().localeCompare(b.fields.Surname.toString())).map(({ fields, id }, index) => (
 								<React.Fragment>
 									{yearGroup ?  (
 										
